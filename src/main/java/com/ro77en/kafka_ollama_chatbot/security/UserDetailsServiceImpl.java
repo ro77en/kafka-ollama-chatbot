b9e9cfed0fa738fb8a51a80e7675f6cb96431a7b
ro77en <lucas.mdantas@icloud.com>
2025-10-29 11:28:50 -0300

    feat: add UserDetails and UserDetailsService implementations

---
+++ src/main/java/com/ro77en/kafka_ollama_chatbot/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ro77en.kafka_ollama_chatbot.security;
+
+import com.ro77en.kafka_ollama_chatbot.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserAuthenticated::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
