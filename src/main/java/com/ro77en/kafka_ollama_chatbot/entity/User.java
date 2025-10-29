744d62750b4d4e0b58bcd1aa2798e4abbb817f9e
ro77en <lucas.mdantas@icloud.com>
2025-10-29 11:27:59 -0300

    feat: add user entity

---
+++ src/main/java/com/ro77en/kafka_ollama_chatbot/entity/User.java
@@ -0,0 +1,23 @@
+package com.ro77en.kafka_ollama_chatbot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    private String password;
+}
