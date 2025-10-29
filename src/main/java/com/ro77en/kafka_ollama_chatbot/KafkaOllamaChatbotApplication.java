559b57820783979888e02a40b6de889d0f11acd7
ro77en <lucas.mdantas@icloud.com>
2025-10-28 23:48:45 -0300

    initial commit

---
+++ src/main/java/com/ro77en/kafka_ollama_chatbot/KafkaOllamaChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.ro77en.kafka_ollama_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOllamaChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOllamaChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ro77en/kafka_ollama_chatbot/KafkaOllamaChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ro77en.kafka_ollama_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOllamaChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
